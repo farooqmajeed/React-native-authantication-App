00941294c4efcd4cbeb14b7fb93c1a1f904c5954
farooqmajeed <farooq306@hotmail.com>
2017-06-20 12:37:37 +0500

    Auth App

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
+
 
 class LoginForm extends Component {
     state ={ text: ''};
@@ -8,10 +8,11 @@ class LoginForm extends Component {
         return (
             <Card>
                 <CardSection>
-                    <TextInput 
+                    <Input 
+                    placeholder=" user@gmail.com"
+                    label="Email"
                     value= {this.state.text}
                     onChangeText= {text => this.setState({ text })}
-                    style={{ height: 20 , width: '100%' }} 
                     /> 
                 </CardSection>
  
