92f8682ba6ed8de0f4eddbc7e0c203a3605f6aab
farooqmajeed <farooq306@hotmail.com>
2017-06-20 14:54:28 +0500

    Auth App

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
-
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-    state ={ text: ''};
+    state = { email: '', password: '' };
+
+    onButtonPress(){
+        const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password); 
+    }
+
+
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Input 
-                    placeholder=" user@gmail.com"
-                    label="Email"
-                    value= {this.state.text}
-                    onChangeText= {text => this.setState({ text })}
-                    /> 
-                </CardSection>
- 
-                <CardSection>
-                
+                    <Input
+                        placeholder=" user@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                        />
                 </CardSection>
 
                 <CardSection>
-                      <Button>
-                         Login
+                    <Input
+                        secureTextEntry
+                        placeholder=" password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        />
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this) }>
+                        Login
                      </Button>
                 </CardSection>
-
-                
             </Card>
         );
     }
-} 
+}
 
 export default LoginForm;
