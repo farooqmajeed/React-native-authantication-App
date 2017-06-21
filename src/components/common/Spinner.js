e1f6179fbaf27156d0b69f497b366a0a1ff55a82
farooqmajeed <farooq306@hotmail.com>
2017-06-21 14:17:05 +0500

    Auth App

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
+const Spinner = ({ size }) => {
     return(
-        <View /> 
+        <View style={styles.spinnerStyle}>
+            <ActivityIndicator size ={ size  || 'large' } />
+        </View> 
+         
     );
-}
-export { Spinner };
+};
+const styles = {
+    spinnerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+};
+export { Spinner }; 
